a831fd425c89e9216b4a1e4574998b2651108245
iqsyal21 <iqsyal21@gmail.com>
2021-08-31 16:44:15 +0700

    authentication implemented

--- src/api/users/handler.js
+++ src/api/users/handler.js
@@ -1,21 +1,21 @@
 const ClientError = require('../../exceptions/ClientError');
- 
+
 class UsersHandler {
   constructor(service, validator) {
     this._service = service;
     this._validator = validator;
- 
+
     this.postUserHandler = this.postUserHandler.bind(this);
     this.getUserByIdHandler = this.getUserByIdHandler.bind(this);
   }
- 
+
     async postUserHandler(request, h) {
     try {
       this._validator.validateUserPayload(request.payload);
       const { username, password, fullname } = request.payload;
- 
+
       const userId = await this._service.addUser({ username, password, fullname });
- 
+
       const response = h.response({
         status: 'success',
         message: 'User berhasil ditambahkan',
@@ -34,7 +34,7 @@ class UsersHandler {
         response.code(error.statusCode);
         return response;
       }
- 
+
       // Server ERROR!
       const response = h.response({
         status: 'error',
@@ -49,13 +49,13 @@ class UsersHandler {
     async getUserByIdHandler(request, h) {
     try {
       const { id } = request.params;
- 
+
       const user = await this._service.getUserById(id);
- 
+
       return {
         status: 'success',
         data: {
-          user
+          user,
         },
       };
     } catch (error) {
@@ -67,7 +67,7 @@ class UsersHandler {
         response.code(error.statusCode);
         return response;
       }
- 
+
       // server ERROR!
       const response = h.response({
         status: 'error',
