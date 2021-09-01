140ebdd1814d142383fcd87c1e89fbe27098eeae
iqsyal21 <iqsyal21@gmail.com>
2021-09-01 17:27:42 +0700

    add foreign key to notes owner column

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,11 +1,11 @@
+/* eslint-disable camelcase */
+
 const mapDBToModel = ({
     id,
     title,
     body,
     tags,
-    // eslint-disable-next-line camelcase
     created_at,
-    // eslint-disable-next-line camelcase
     updated_at,
   }) => ({
     id,
